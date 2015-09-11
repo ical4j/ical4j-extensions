c9e74aba6ac1057b3bec2d6d3ac881cd5b7dd4ce
Ben Fortuna <bfortuna@odecee.com.au>
2015-09-12 00:26:35 +1000

    Added calext draft properties

---
+++ src/main/java/net/fortuna/ical4j/extensions/property/Conference.java
@@ -0,0 +1,51 @@
+package net.fortuna.ical4j.extensions.property;
+
+import net.fortuna.ical4j.model.*;
+
+/**
+ * Created by fortuna on 11/09/15.
+ */
+public class Conference extends Property {
+
+    private static final long serialVersionUID = 1L;
+
+    public static final String PROPERTY_NAME = "CONFERENCE";
+
+    private String value;
+
+    public Conference() {
+        super(PROPERTY_NAME, PropertyFactoryImpl.getInstance());
+    }
+
+    public Conference(ParameterList params, String value) {
+        super(PROPERTY_NAME, params, PropertyFactoryImpl.getInstance());
+        setValue(value);
+    }
+
+    @Override
+    public void setValue(String aValue) {
+        this.value = aValue;
+    }
+
+    @Override
+    public void validate() throws ValidationException {
+
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static class Factory extends Content.Factory implements PropertyFactory<Conference> {
+
+        public Conference createProperty() {
+            return new Conference();
+        }
+
+        public Conference createProperty(ParameterList parameters, String value) {
+            Conference property = new Conference(parameters, value);
+            return property;
+        }
+    }
+}
