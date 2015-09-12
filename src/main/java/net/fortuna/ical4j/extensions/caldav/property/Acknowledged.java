4c01af83aed158b1ec84d956b09834d6ac8495a2
Ben Fortuna <bfortuna@odecee.com.au>
2015-09-13 00:34:42 +1000

    Added factory support for Acknowledged property

--- src/main/java/net/fortuna/ical4j/extensions/caldav/property/Acknowledged.java
+++ src/main/java/net/fortuna/ical4j/extensions/caldav/property/Acknowledged.java
@@ -1,22 +1,22 @@
 /**
  * Copyright (c) 2012, Ben Fortuna
  * All rights reserved.
- *
+ * <p/>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- *
- *  o Redistributions of source code must retain the above copyright
+ * <p/>
+ * o Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- *
- *  o Redistributions in binary form must reproduce the above copyright
+ * <p/>
+ * o Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- *
- *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * <p/>
+ * o Neither the name of Ben Fortuna nor the names of any other contributors
  * may be used to endorse or promote products derived from this software
  * without specific prior written permission.
- *
+ * <p/>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -31,72 +31,83 @@
  */
 package net.fortuna.ical4j.extensions.caldav.property;
 
-import java.text.ParseException;
-
-import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.ParameterList;
-import net.fortuna.ical4j.model.PropertyFactoryImpl;
+import net.fortuna.ical4j.model.*;
 import net.fortuna.ical4j.model.property.UtcProperty;
+
+import java.text.ParseException;
 
 /**
  * This class supports the new ACKNOWLEDGED property, as defined in draft-daboo-valarm-extensions
- * 
+ *
  * @see <a href="http://tools.ietf.org/html/draft-daboo-valarm-extensions-04">draft-daboo-valarm-extensions-04</a>
- * 
+ *
  * @author probert
  *
  */
 public class Acknowledged extends UtcProperty {
 
-  private static final long serialVersionUID = 2182103734645261668L;
-  
-  public static final String PROPERTY_NAME = "ACKNOWLEDGED";
-    
-  /**
-   * Default constructor. Initialises the dateTime value to the time of instantiation.
-   */
-  public Acknowledged() {
-      super(PROPERTY_NAME, PropertyFactoryImpl.getInstance());
-  }
+    private static final long serialVersionUID = 2182103734645261668L;
 
-  /**
-   * @param aValue a string representation of a DTSTAMP value
-   * @throws ParseException if the specified value is not a valid representation
-   */
-  public Acknowledged(final String aValue) throws ParseException {
-    this(new ParameterList(), aValue);
-  }
-  
-  /**
-   * @param aList a list of parameters for this component
-   * @param aValue a value string for this component
-   * @throws ParseException where the specified value string is not a valid date-time/date representation
-   */
-  public Acknowledged(final ParameterList aList, final String aValue)
-          throws ParseException {
-      super(PROPERTY_NAME, aList, PropertyFactoryImpl.getInstance());
-      setValue(aValue);
-  }
+    public static final String PROPERTY_NAME = "ACKNOWLEDGED";
 
-  /**
-   * @param aDate a date representing a date-time 
-   */
-  public Acknowledged(final DateTime aDate) {
-      super(PROPERTY_NAME, PropertyFactoryImpl.getInstance());
-      // time must be in UTC..
-      aDate.setUtc(true);
-      setDate(aDate);
-  }
+    /**
+     * Default constructor. Initialises the dateTime value to the time of instantiation.
+     */
+    public Acknowledged() {
+        super(PROPERTY_NAME, PropertyFactoryImpl.getInstance());
+    }
 
-  /**
-   * @param aList a list of parameters for this component
-   * @param aDate a date representing a date-time
-   */
-  public Acknowledged(final ParameterList aList, final DateTime aDate) {
-      super(PROPERTY_NAME, aList, PropertyFactoryImpl.getInstance());
-      // time must be in UTC..
-      aDate.setUtc(true);
-      setDate(aDate);
-  }
+    /**
+     * @param aValue a string representation of a DTSTAMP value
+     * @throws ParseException if the specified value is not a valid representation
+     */
+    public Acknowledged(final String aValue) throws ParseException {
+        this(new ParameterList(), aValue);
+    }
 
+    /**
+     * @param aList  a list of parameters for this component
+     * @param aValue a value string for this component
+     * @throws ParseException where the specified value string is not a valid date-time/date representation
+     */
+    public Acknowledged(final ParameterList aList, final String aValue)
+            throws ParseException {
+        super(PROPERTY_NAME, aList, PropertyFactoryImpl.getInstance());
+        setValue(aValue);
+    }
+
+    /**
+     * @param aDate a date representing a date-time
+     */
+    public Acknowledged(final DateTime aDate) {
+        super(PROPERTY_NAME, PropertyFactoryImpl.getInstance());
+        // time must be in UTC..
+        aDate.setUtc(true);
+        setDate(aDate);
+    }
+
+    /**
+     * @param aList a list of parameters for this component
+     * @param aDate a date representing a date-time
+     */
+    public Acknowledged(final ParameterList aList, final DateTime aDate) {
+        super(PROPERTY_NAME, aList, PropertyFactoryImpl.getInstance());
+        // time must be in UTC..
+        aDate.setUtc(true);
+        setDate(aDate);
+    }
+
+    public static class Factory extends Content.Factory implements PropertyFactory<Acknowledged> {
+        public Factory() {
+            super(PROPERTY_NAME);
+        }
+
+        public Acknowledged createProperty() {
+            return new Acknowledged();
+        }
+
+        public Acknowledged createProperty(ParameterList parameters, String value) throws ParseException {
+            return new Acknowledged(parameters, value);
+        }
+    }
 }
