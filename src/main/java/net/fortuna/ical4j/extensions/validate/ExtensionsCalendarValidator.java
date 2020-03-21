ae65e866b1585f553c8713fc7557ba65a1e89c26
Ben Fortuna <bfortuna@odecee.com.au>
2020-03-21 14:22:52 +1100

    Removed properties added to core ical4j lib

--- src/main/java/net/fortuna/ical4j/extensions/validate/ExtensionsCalendarValidator.java
+++ src/main/java/net/fortuna/ical4j/extensions/validate/ExtensionsCalendarValidator.java
@@ -1,12 +1,9 @@
 package net.fortuna.ical4j.extensions.validate;
 
-import net.fortuna.ical4j.extensions.property.Name;
 import net.fortuna.ical4j.extensions.property.*;
-import net.fortuna.ical4j.model.property.*;
 import net.fortuna.ical4j.validate.CalendarValidatorImpl;
 import net.fortuna.ical4j.validate.ValidationRule;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -15,10 +12,9 @@ import java.util.Collections;
 public class ExtensionsCalendarValidator extends CalendarValidatorImpl {
 
     public ExtensionsCalendarValidator(ValidationRule... rules) {
-        super(Arrays.asList(rules));
+        super(rules);
 
-        Collections.addAll(calendarProperties, WrTimezone.class, WrRelCalId.class, CalStart.class, WrCalDesc.class, WrCalName.class,
-            // calext
-                Name.class, Description.class, Uid.class, LastModified.class, Url.class, Categories.class);
+        Collections.addAll(calendarProperties, WrTimezone.class, WrRelCalId.class, CalStart.class, WrCalDesc.class,
+                WrCalName.class);
     }
 }
