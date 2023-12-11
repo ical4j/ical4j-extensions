bf2c6847111cab2ec906b145c47c83a94c98ced5
Ben Fortuna <fortuna@micronode.com>
2023-12-11 16:24:17 +1100

    Added common link types

---
+++ src/main/java/net/fortuna/ical4j/extensions/link/License.java
@@ -0,0 +1,20 @@
+package net.fortuna.ical4j.extensions.link;
+
+import net.fortuna.ical4j.model.LinkRelationType;
+import net.fortuna.ical4j.model.parameter.LinkRel;
+import net.fortuna.ical4j.model.property.Link;
+
+import java.net.URI;
+
+/**
+ * Refers to a license associated with this context.
+ * <p>
+ * See also <a href="https://www.iana.org/assignments/link-relations/link-relations.xhtml">Link Relations</a>
+ */
+public class License extends Link {
+
+    public License(URI uri) {
+        super(uri);
+        add(new LinkRel(LinkRelationType.license));
+    }
+}
