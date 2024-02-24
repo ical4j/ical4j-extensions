9a778c7f89deb727672596a604d54917309c49b4
Ben Fortuna <fortuna@micronode.com>
2024-02-24 13:30:39 +1100

    Added module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,23 @@
 module ical4j.extensions {
     requires java.base;
-    requires org.mnode.ical4j.core;
+    requires ical4j.core;
     requires ical4j.vcard;
+
     requires org.threeten.extra;
+    requires static org.codehaus.groovy;
+
+    exports net.fortuna.ical4j.extensions.alarm;
+    exports net.fortuna.ical4j.extensions.caldav.parameter;
+    exports net.fortuna.ical4j.extensions.caldav.property;
+    exports net.fortuna.ical4j.extensions.concept;
+    exports net.fortuna.ical4j.extensions.data;
+    exports net.fortuna.ical4j.extensions.groupwise;
+    exports net.fortuna.ical4j.extensions.link;
+    exports net.fortuna.ical4j.extensions.location;
+    exports net.fortuna.ical4j.extensions.lotus;
+    exports net.fortuna.ical4j.extensions.outlook;
+    exports net.fortuna.ical4j.extensions.parameter;
+    exports net.fortuna.ical4j.extensions.participant;
+    exports net.fortuna.ical4j.extensions.property;
+    exports net.fortuna.ical4j.extensions.validate;
 }
